2846c3f89c2311794dae67f0932ef53e71b23ebd
witold-gawlowski <witek.gawlowski@gmail.com>
2016-11-25 09:15:31 +0000

    Initial commit

---
+++ Source/LUnreal/LUnreal.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "LUnreal.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, LUnreal, "LUnreal" );
---
+++ Source/LUnreal/LUnrealGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "LUnrealGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LUNREAL_API ALUnrealGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
